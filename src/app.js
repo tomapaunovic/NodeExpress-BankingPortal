1477583ac884f46abdc583119eb65d61a0535493
Toma Paunovic <paunovic@work.co>
2020-05-22 14:15:08 +0200

    Module3

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync(
   path.join(__dirname, "json/accounts.json"),
@@ -30,7 +31,31 @@ app.get("/credit", function (req, res) {
   res.render("account", { account: accounts.credit });
 });
 app.get("/profile", function (req, res) {
-    res.render("profile", { user: users[0] });
+  res.render("profile", { user: users[0] });
+});
+app.get("/transfer", function (req, res) {
+  res.render("transfer");
+});
+app.post("/transfer", function (req, res) {
+  let { from, to, amount } = req.body;
+  amount = parseInt(amount);
+  accounts[from].balance -= amount;
+  accounts[to].balance += amount;
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, "UTF-8");
+  res.render("transfer", {message: "Transfer Completed"});
+});
+app.get("/payment", function (req, res) {
+    res.render("payment", {account: accounts.credit});
+  });
+  app.post("/payment", function (req, res) {
+    let { amount } = req.body;
+    amount = parseInt(amount);
+    accounts.credit.balance -= amount;
+    accounts.credit.available += amount;
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, "UTF-8");
+    res.render("payment", { message: "Payment Successful", account: accounts.credit });
   });
 
 app.listen(3000, () =>
